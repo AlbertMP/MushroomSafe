890c4306647e009256ce5ef84767e491118db5ac
Peng Miao <miaomiao315@hotmail.com>
2023-06-03 01:52:00 +0100

    Modify flask

--- flask/app.py
+++ flask/app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 @app.route('/classify', methods=['POST'])
 def classify_image():
     file = request.files['image']
-    image = your_model_module.preprocess_image(file)  # 预处理图像
-    result = your_model_module.predict(image)  # 使用您的模型进行预测
+    image = your_model_module.preprocess_image(file)
+    result = your_model_module.predict(image)
     return jsonify(result)
 
 if __name__ == '__main__':
